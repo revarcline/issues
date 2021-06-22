f17f490b0243cd3c1e8ae88a26b6316be6dadfdd
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-22 14:41:26 -0500

    hm hm

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,22 @@
+defmodule Issues.GithubIssues do
+  @moduledoc "http handling"
+  @user_agent [{"User-agent", "Elixir rev.a.r.cline@gmail.com"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "http://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  def handle_response({_, %{status_code: _, body: body}}) do
+    {:error, body}
+  end
+end
