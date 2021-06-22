f159df674aff86780cefc166610c3c7b35b0295c
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-21 19:41:18 -0500

    added httpoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -21,24 +23,33 @@ defmodule Issues.CLI do
   was given.
   """
   def parse_args(argv) do
-    parse =
-      OptionParser.parse(argv,
-        switches: [help: :boolean],
-        aliases: [h: :help]
-      )
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _, _} ->
-        :help
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
 
-      {_, [user, project, count], _} ->
-        {user, project, String.to_integer(count)}
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
 
-      {_, [user, project], _} ->
-        {user, project, @default_count}
+  def args_to_internal_representation(_) do
+    :help
+  end
 
-      _ ->
-        :help
-    end
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
+  end
+
+  def process([user, project, _count]) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
