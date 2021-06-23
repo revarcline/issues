4a425b9cdbd3904eb17fe7dc2bc3a409ef1563ec
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-23 13:10:08 -0500

    ok now we use-a the count

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,10 +49,11 @@ defmodule Issues.CLI do
     """)
   end
 
-  def process([user, project, _count]) do
+  def process([user, project, count]) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_descending_order()
+    |> last(count)
   end
 
   def decode_response({:ok, body}), do: body
@@ -60,6 +61,12 @@ defmodule Issues.CLI do
   def decode_response({:error, error}) do
     IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 
   def sort_into_descending_order(list_of_issues) do
